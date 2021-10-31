a79cffe45d0a5162cef28df3c628d32b4e4c7674
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-31 05:33:05 +0100

    sing in and register page design changed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -64,74 +64,83 @@ const Login = () => {
       });
   };
   return (
-    <div className="bg-blue-100 min-h-screen">
-      <div className="container py-2">
-        <div className="bg-gray-50 my-2 py-5 text-center rounded-md shadow-md">
-          <h3 className="text-4xl text-gray-800 font-medium capitalize">
-            Login page
-          </h3>
-        </div>
-        {/* form */}
-        <div className="container py-5 md:w-3/6">
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
-            {/* Email */}
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              {...register('email', { required: true })}
-              required
-              className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
-            />
-            <p className="text-red-700">
-              {errors.email && <span>This field is required</span>}
-            </p>
-            {/* Password */}
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              {...register('password', { required: true, min: 6 })}
-              name="password"
-              required
-              className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
-            />
-            <p className="text-red-700">
-              {errors.password && <span>This field is required</span>}
-            </p>
-            {/* Button */}
-            <button className="bg-blue-800 hover:bg-blue-900 transition delay-75 text-gray-100 py-2 px-2.5 w-full my-1.5 rounded-md shadow-md">
-              Login
-            </button>
-          </form>
-        </div>
-
-        <div className="text-center">
-          <button
-            className="bg-red-700 py-2 px-5 text-white capitalize rounded-md shadow-md"
-            onClick={handleGoogleSignIn}
+    <div className="bg-gray-100">
+      <div
+        style={{
+          background: `linear-gradient(0deg
+        , #11182773, #111827), url("https://i.ibb.co/1MDR7Gp/contact-bg.jpg")`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundAttachment: 'fixed',
+          backgroundPosition: 'center',
+          height: '30vh',
+        }}
+        className="flex justify-center items-center"
+      >
+        <h2 className="text-center py-5 text-gray-100 text-3xl font-medium">
+          Log In
+        </h2>
+      </div>
+      {/* form */}
+      <div className="container py-5 md:w-3/6">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+          {/* Email */}
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
           >
-            Sign In with Google
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            {...register('email', { required: true })}
+            required
+            className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
+          />
+          <p className="text-red-700">
+            {errors.email && <span>This field is required</span>}
+          </p>
+          {/* Password */}
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            {...register('password', { required: true, min: 6 })}
+            name="password"
+            required
+            className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
+          />
+          <p className="text-red-700">
+            {errors.password && <span>This field is required</span>}
+          </p>
+          {/* Button */}
+          <button className="bg-blue-800 hover:bg-blue-900 transition delay-75 text-gray-100 py-2 px-2.5 w-full my-1.5 rounded-md shadow-md">
+            Login
           </button>
-          <p className="my-4">Or</p>
-          <Link
-            className="bg-blue-700 py-2 px-5 text-white capitalize rounded-md shadow-md"
-            to="/register"
-          >
-            Register Here
-          </Link>
-        </div>
+        </form>
+      </div>
+
+      <div className="text-center pb-10">
+        <button
+          className="bg-red-700 py-2 px-5 text-white capitalize rounded-md shadow-md"
+          onClick={handleGoogleSignIn}
+        >
+          Sign In with Google
+        </button>
+        <p className="my-4">Or</p>
+        <Link
+          className="bg-blue-700 py-2 px-5 text-white capitalize rounded-md inline-block shadow-md"
+          to="/register"
+        >
+          Register Here
+        </Link>
       </div>
     </div>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory, useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 
 const Register = () => {
@@ -63,84 +64,99 @@ const Register = () => {
   };
 
   return (
-    <div className="bg-blue-100 min-h-screen">
-      <div className="container py-2">
-        <div className="bg-gray-50 my-2 py-5 text-center rounded-md shadow-md">
-          <h3 className="text-4xl text-gray-800 font-medium capitalize">
-            Register Here
-          </h3>
-        </div>
-        <div className="container py-5 md:w-3/6">
-          <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
-            {/* Name */}
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              {...register('name', { required: true })}
-              required
-              className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
-            />
-            <p className="text-red-700">
-              {errors.name && <span>This field is required</span>}
-            </p>
-            {/* Email */}
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              {...register('email', { required: true })}
-              required
-              className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
-            />
-            <p className="text-red-700">
-              {errors.email && <span>This field is required</span>}
-            </p>
-            {/* Password */}
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              {...register('password', { required: true, min: 6 })}
-              name="password"
-              required
-              className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
-            />
-            <p className="text-red-700">
-              {errors.password && <span>This field is required</span>}
-            </p>
-            {/* Button */}
-            <button className="bg-blue-800 hover:bg-blue-900 transition delay-75 text-gray-100 py-2 px-2.5 w-full my-1.5 rounded-md shadow-md">
-              Register
-            </button>
-          </form>
-          {/* Other sign in */}
-          <div className="text-center my-5">
-            <p>or</p>
-            <button
-              className="bg-red-700 py-2 px-5 text-white capitalize rounded-md shadow-md"
-              onClick={handleGoogleSignIn}
-            >
-              Sign In with Google
-            </button>
-          </div>
+    <div className="bg-gray-100">
+      <div
+        style={{
+          background: `linear-gradient(0deg
+        , #11182773, #111827), url("https://i.ibb.co/1MDR7Gp/contact-bg.jpg")`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundAttachment: 'fixed',
+          backgroundPosition: 'center',
+          height: '30vh',
+        }}
+        className="flex justify-center items-center"
+      >
+        <h2 className="text-center py-5 text-gray-100 text-3xl font-medium">
+          Register
+        </h2>
+      </div>
+      <div className="container py-5 md:w-3/6">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+          {/* Name */}
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            {...register('name', { required: true })}
+            required
+            className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
+          />
+          <p className="text-red-700">
+            {errors.name && <span>This field is required</span>}
+          </p>
+          {/* Email */}
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            {...register('email', { required: true })}
+            required
+            className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
+          />
+          <p className="text-red-700">
+            {errors.email && <span>This field is required</span>}
+          </p>
+          {/* Password */}
+          <label
+            htmlFor="password"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            {...register('password', { required: true, min: 6 })}
+            name="password"
+            required
+            className="mt-1 p-2 border block w-full shadow sm:text-sm border-gray-200 outline-none focus:border-blue-500  ring-gray-400 rounded-md"
+          />
+          <p className="text-red-700">
+            {errors.password && <span>This field is required</span>}
+          </p>
+          {/* Button */}
+          <button className="bg-blue-800 hover:bg-blue-900 transition delay-75 text-gray-100 py-2 px-2.5 w-full my-1.5 rounded-md shadow-md">
+            Register
+          </button>
+        </form>
+        {/* Other sign in */}
+        <div className="text-center my-5">
+          <p>or</p>
+          <button
+            className="bg-red-700 py-2 px-5 text-white capitalize rounded-md shadow-md"
+            onClick={handleGoogleSignIn}
+          >
+            Sign In with Google
+          </button>
+          <Link
+            className="bg-blue-700 py-2 px-5 mx-3 text-white capitalize rounded-md inline-block shadow-md"
+            to="/login"
+          >
+            Sign In with Email
+          </Link>
         </div>
       </div>
     </div>
