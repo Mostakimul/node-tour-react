3bd8decbf91686029a31f18ec1e6d619fe621c9b
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-10-31 05:25:17 +0100

    changed detination grid

--- src/components/destination/Destinantions.js
+++ src/components/destination/Destinantions.js
@@ -25,7 +25,7 @@ const Destinantions = () => {
 
   return (
     <div className=" py-5">
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
         {load ? (
           <div>
             <Loader
